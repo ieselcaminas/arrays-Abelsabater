5a3dc6598de04e9bd649fc90a56a261bdd5391c6
Abelsabater <abesabmun@alu.edu.gva.es>
2024-10-24 13:42:31 +0200

    Tema3

--- src/main/Tema3/IMPRIMIR_ARRAY.java
+++ src/main/Tema3/IMPRIMIR_ARRAY.java
@@ -2,22 +2,26 @@ package Tema3;
 
 public class IMPRIMIR_ARRAY {
     public static void main(String[] args) {
-        int [] anArray;
+        int[] anArray;
 
-        anArray=new int [10];
-        anArray[0]=1000;
-        anArray[1]=900;
-        anArray[2]=800;
-        anArray[3]=700;
-        anArray[4]=600;
-        anArray[5]=500;
-        anArray[6]=400;
-        anArray[7]=300;
-        anArray[8]=200;
-        anArray[9]=100;
-        for (int i = 0; i < 10; i++) {
-            System.out.println(anArray[i]);
+        anArray = new int[10];
+
+        // initialize first element
+        anArray[0] = 100;
+        // initialize second element
+        anArray[1] = 200;
+        // and so forth
+        anArray[2] = 300;
+        anArray[3] = 400;
+        anArray[4] = 500;
+        anArray[5] = 600;
+        anArray[6] = 700;
+        anArray[7] = 800;
+        anArray[8] = 900;
+        anArray[9] = 1000;
+
+        for (int i = 0; i < anArray.length; i++) {
+            System.out.println("Element at index "+ i+": " + anArray[i]);
         }
-
     }
 }
