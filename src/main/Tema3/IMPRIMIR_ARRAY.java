770d669037bc00bb8b0c51b702561b677c0bc2b5
Abelsabater <abesabmun@alu.edu.gva.es>
2024-10-24 13:38:31 +0200

    Tema3

---
+++ src/main/Tema3/IMPRIMIR_ARRAY.java
@@ -0,0 +1,23 @@
+package Tema3;
+
+public class IMPRIMIR_ARRAY {
+    public static void main(String[] args) {
+        int [] anArray;
+
+        anArray=new int [10];
+        anArray[0]=1000;
+        anArray[1]=900;
+        anArray[2]=800;
+        anArray[3]=700;
+        anArray[4]=600;
+        anArray[5]=500;
+        anArray[6]=400;
+        anArray[7]=300;
+        anArray[8]=200;
+        anArray[9]=100;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(anArray[i]);
+        }
+
+    }
+}
